ae116808b424e3bdcf9c97a17d45f9e3ec8010d6
kirtan227 <kirtan227@gmail.com>
2016-10-07 21:21:52 +0530

    Add files via upload

---
+++ matrix_inverse_tb.v
@@ -0,0 +1,21 @@
+
+
+module matrix_inverse_tb;
+
+	// Inputs
+	reg clk;
+
+	// Instantiate the Unit Under Test (UUT)
+	matrix_inverse uut (
+		.clk(clk)
+	);
+
+	always #50 clk = ~clk;
+	initial
+	begin
+		clk = 0;
+	end
+
+      
+endmodule
+
